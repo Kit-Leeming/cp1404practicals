e6b96aeabe7fd078725d06a493e6fd9e2c4aed2a
Kit Leeming <Kit.leeming@my.jcu.edu.au>
2025-10-12 13:45:52 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,21 @@
-minimum_password_length = 8
-password = input("Password: ")
-while len(password) < minimum_password_length:
-    print("Password too short")
+
+
+def main():
+    minimum_password_length = 8
+    password = get_password(minimum_password_length)
+    print_hidden_password(password)
+
+
+def print_hidden_password(password: str, character="*"):
+    print(character * len(password))
+
+
+def get_password(minimum_password_length: int) -> str:
     password = input("Password: ")
-print("*" * len(password))
+    while len(password) < minimum_password_length:
+        print("Password too short")
+        password = input("Password: ")
+    return password
+
+
+main()
