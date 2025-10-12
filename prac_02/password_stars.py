97218102cd564cf58119705d84ebfebc5a0219f8
Kit Leeming <Kit.leeming@my.jcu.edu.au>
2025-10-12 13:39:33 +1000

    Add password check program

---
+++ prac_02/password_stars.py
@@ -0,0 +1,6 @@
+minimum_password_length = 8
+password = input("Password: ")
+while len(password) < minimum_password_length:
+    print("Password too short")
+    password = input("Password: ")
+print("*" * len(password))
