d203db1e0815fc36bfdd2c8831186c4b31c96b42
Kit Leeming <Kit.leeming@my.jcu.edu.au>
2025-10-12 18:06:06 +1000

    Implemented randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,21 @@
+
+import random
+# What did you see on line 1?
+# 10, 12, 18, 8, 12
+# What was the smallest number you could have seen, what was the largest?
+# 5 is the smallest and 20 is the largest
+
+# What did you see on line 2?
+# 9, 7, 9, 7, 3
+# What was the smallest number you could have seen, what was the largest?
+# 3 is the smallest and 9 is the largest
+# Could line 2 have produced a 4?
+# No, as it has a step of 2 from 3 (therefore only odd numbers)
+
+# What did you see on line 3?
+# 3.1997305796560642, 5.025014782527986, 2.75910605699329, 5.3735104396399045
+# What was the smallest number you could have seen, what was the largest?
+# 2.5 is the smallest and 5 is the largest
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+print(random.randint(1, 100))
