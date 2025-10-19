9102c24cb2d169437a29add02d2c57afb9d13ac1
Kit Leeming <Kit.leeming@my.jcu.edu.au>
2025-10-19 12:55:25 +1000

    Change list_exercises.py to take an indefinite number of inputs until the user enters a negative value

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,7 +1,5 @@
 """List exercises for CP1404"""
 
-NUMBER_OF_NUMBERS = 5
-
 
 def main():
     numbers = []
@@ -15,15 +13,28 @@ def main():
     else:
         print("Access denied")
 
-    for i in range(NUMBER_OF_NUMBERS):
-        number = int(input(f"Enter number {i + 1}: "))
+    number = get_valid_number("Enter number 1: ", int)
+    while number >= 0:
         numbers.append(number)
+        number = get_valid_number(f"Enter number {len(numbers) + 1}: ", int)
 
     print(f"The fist number is {numbers[0]}")
     print(f"The last number is {numbers[-1]}")
     print(f"The smallest number is {min(numbers)}")
     print(f"The largest number is {max(numbers)}")
     print(f"The average of numbers is {sum(numbers) / len(numbers)}")
+
+
+def get_valid_number(print_message, number_type):
+    """Get a valid number from the user"""
+    is_valid_input = False
+    while not is_valid_input:
+        try:
+            number = number_type(input(print_message))
+            is_valid_input = True
+        except ValueError:
+            print("Invalid input")
+    return number  # no problem with reference before assignment
 
 
 main()
