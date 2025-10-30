ef69e2de6da4993a8e6189e73a86cf1636e0bc82
Kit Leeming <Kit.leeming@my.jcu.edu.au>
2025-10-30 14:09:34 +1000

    Implement PR and feedback changes to hex_colours.py

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -18,32 +18,35 @@ def main():
             print(colour, "is", COLOUR_NAME_TO_HEX_CODE[colour])
         except KeyError:
             print("Invalid colour")
-        colour = input("Enter short state: ").title()
+        colour = input("Enter colour: ").title()
 
+
+"""Extended work below"""
 
 def generate_colour_dictionary():
+    """Generate colour dictionary from colour hex website"""
     start_string = "<tbody><tr><td>"
     end_string = "</tbody"
 
     response = requests.get("https://www.color-hex.com/color-names.html")
     text = response.text
 
-    start_index = text.find(start_string) + len(start_string)
+    start_index = text.find(start_string) + len(start_string)  # Find start of colour codes on page
     finish_index = text.find(end_string)
 
-    section = text[start_index:finish_index]
-    section = section.split("<tr><td>")
+    section = text[start_index:finish_index]  # Extract section of web page that contains colour information
+    colours = section.split("<tr><td>")  # Split into list with each colour as an entry
 
-    pattern = re.compile(r"^(.*?)</td>.*?>(#[0-9a-fA-F]{6})<", re.DOTALL)
+    pattern = re.compile(r"^(.*?)</td>.*?>(#[0-9a-fA-F]{6})<", re.DOTALL)  # Create pattern for colour information
     color_to_hex_code = {}
 
-    for row in section:
-        match = pattern.search(row)
+    for colour in colours:
+        match = pattern.search(colour)
         if match:
-            name, color = match.groups()
+            name, color = match.groups()  # Extract the colour and hex code
             color_to_hex_code[name.strip()] = color
 
-    for i in range(800):
+    for i in range(800):  # Print the first 800 colours and their codes
         print(f"'{list(color_to_hex_code)[i]}': '{list(color_to_hex_code.values())[i]}'")
 
 
