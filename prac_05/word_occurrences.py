f1b612842aa894152fc7afe75c0cf51b249c6359
Kit Leeming <Kit.leeming@my.jcu.edu.au>
2025-10-30 14:07:02 +1000

    Implement PR and feedback changes to word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -14,7 +14,7 @@ def main():
         # If word exists in dictionary return count, if not return 0
         word_to_count[cleaned_word] = word_to_count.get(cleaned_word, 0) + 1
 
-    max_word_length = max(len(key) for key in word_to_count)
+    max_word_length = max(len(word) for word in word_to_count)
 
     for word, count in word_to_count.items():
         print(f"{word:<{max_word_length}} : {count}")
