f86f638ebf1a1706974906ee1ef17ddcb28bf899
Kit Leeming <Kit.leeming@my.jcu.edu.au>
2025-10-30 14:06:39 +1000

    Implement PR and feedback changes to emails.py

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -8,31 +8,28 @@ import re
 
 
 def main():
-    quit_program = False
     name_to_email = {}
-    while not quit_program:
-        full_email = str(input("Email: "))
-        if full_email == '':
-            quit_program = True
-        else:
-            local_email_username = full_email.split("@")[0]  # Remove email host
-            local_email_username = re.sub(r"\d+", "", local_email_username)  # Remove any non letter characters
-            names = re.split(r"[-._]+", local_email_username)  # Split username at common delimiters
-            full_name = " ".join(names).title()
-
-            user_confirmation = input(f"Is your name {full_name}? (y/n)").lower()
-            if user_confirmation == "y" or user_confirmation == "":
-                name_to_email[full_name] = full_email
-            elif user_confirmation == "n":
-                full_name = input("Name: ")
-                name_to_email[full_name] = full_email
-            else:
-                print("Invalid selection, please try again")
+    full_email = input("Email: ")
+    while full_email != "":
+        full_name = extract_name(full_email)
+        user_confirmation = input(f"Is your name {full_name}? (y/n)").lower()
+        if user_confirmation != "y" or user_confirmation != "":
+            full_name = input("Name: ")
+        name_to_email[full_name] = full_email
+        print("Invalid selection, please try again")
 
     max_name_length = max(len(name) for name in name_to_email)
 
     for name, email in name_to_email.items():
         print(f"{name:<{max_name_length}} ({email})")
+
+
+def extract_name(full_email: str):
+    local_email_username = full_email.split("@")[0]  # Remove email host
+    local_email_username = re.sub(r"\d+", "", local_email_username)  # Remove any non letter characters
+    names = re.split(r"[-._]+", local_email_username)  # Split username at common delimiters
+    full_name = " ".join(names).title()
+    return full_name
 
 
 main()
