8b46d4cbd3fc8a23f294751f4a624f2105ba4c26
Kit Leeming <Kit.leeming@my.jcu.edu.au>
2025-11-01 13:09:35 +1000

    Complete programming language exercise

---
+++ prac_06/programming_language.py
@@ -0,0 +1,20 @@
+"""CP1404/CP5632 Practical - programming language class."""
+
+
+class ProgrammingLanguage:
+    """Represent a programming language as an object"""
+
+    def __init__(self, name="", typing="", reflection=False, year=0):
+        """Initialise a programming language instance"""
+        self.name = name.title()
+        self.typing = typing.title()
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Determine if programming language has dynamic typing"""
+        return self.typing.lower() == "dynamic"
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
